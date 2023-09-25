36b61a63c562d60c2635582236303b3a4662c557
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 02:21:10 +0600

    o

--- variable.js
+++ variable.js
@@ -1,7 +1,18 @@
 // vary+able=variable
+// Numeric
 var price=11;
 var age=25;
 var temperature=30;
 var wait=59;
 var area=100;
 var sons=4;
+
+//String
+var name="Sagor Alam";
+var location='Rangpur';
+var star='aliya bhate';
+
+// bollean
+var serious=true;
+var isFullMark=false;
+var isSingle=true;
