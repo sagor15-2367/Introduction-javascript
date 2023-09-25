9e6de1a14ed9e7e4a5f39ec7082df3e0b21cdffb
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-09-26 02:56:12 +0600

    ok

---
+++ operation.js
@@ -0,0 +1,13 @@
+var orange=20;
+var apple=10;
+var totalCost=orange+apple;
+var substraction=orange-apple;
+var multiple=orange*apple;
+var division=orange/apple;
+
+
+// outpur
+console.log(totalCost);
+console.log(substraction);
+console.log(multiple);
+console.log(division);
